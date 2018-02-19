4c3692977742d79c1998d8fb731744f7efb0182f
Josue Brito <josue.brito.j@gmail.com>
2018-02-18 23:48:22 -0300

    First version

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,12 +5,47 @@ import { Http } from "@angular/http";
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando:boolean = true;
 
 
   constructor( private http:Http ) {
 
     this.cargar_productos();
+
+
+    }
+
+    public buscar_producto( termino:string ){
+
+      // console.log("Buscando producto");
+      // console.log( this.productos.length );
+
+      if( this.productos.length === 0){
+        this.cargar_productos().then( ()=>{
+          //termino la carga
+          this.filtrar_productos(termino);
+        });
+      }else{
+        this.filtrar_productos(termino);
+      }
+    }
+
+    private filtrar_productos(termino:string){
+
+      this.productos_filtrado = [];
+
+      termino = termino.toLowerCase();
+
+      this.productos.forEach( prod =>{
+
+        if( prod.categoria.indexOf( termino ) >=0 || prod.titulo.toLowerCase().indexOf( termino ) >=0 ){
+          this.productos_filtrado.push( prod );
+        }
+        // console.log( prod );
+        // console.log( prod );
+
+      })
 
 
     }
@@ -25,6 +60,9 @@ export class ProductosService {
 
      this.cargando = true;
 
+     let promesa = new Promise( ( resolve, reject )=>{
+
+
        this.http.get('https://opsdemo-a160b.firebaseio.com/productos_idx.json')
             .subscribe( res => {
 
@@ -34,8 +72,17 @@ export class ProductosService {
 
             this.cargando =false;
             this.productos = res.json ();
+            resolve();
           // }, 1500 )
-        })
+        });
+
+
+
+     });
+
+     return promesa;
+
+
 
      }
    }
